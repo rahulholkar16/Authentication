0aba3aa5b1809a7c2ab800f7d696dda8709cadc0
rahulholkar16 <usefulltech1819@gmail.com>
2026-03-11 15:01:10 +0530

    Add App.ts intial setup

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,15 @@
 import express from "express";
+import cookiesParser from "cookie-parser";
 
 const app = express();
 app.use(express.json({ limit: "16kb" }));
-app.use()
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(cookiesParser());
+
+app.get("/", (req, res) => {
+    res.json({
+        msg: "Welcome, back"
+    })
+});
+
+export default app;
